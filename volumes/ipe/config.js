9dcf8f5264dc35500157d2df3d2a67fdb758ea7b
Antonio Nardella <ioiobzit@users.noreply.github.com>
2018-02-23 11:39:17 +0100

    updated config to new version

--- volumes/ipe/config.js
+++ volumes/ipe/config.js
@@ -5,9 +5,6 @@ module.exports = {
     // url and port of your IRI node
     iota_node_url: 'http://iota:14265',
 
-    // keep this
-    stresstest_table_url: 'http://analytics.iotaledger.net/stresstest.table',
-
     // address and port where the exporter will be bound
     bind_address: '0.0.0.0',
     bind_port: 9311,
@@ -19,4 +16,18 @@ module.exports = {
     // if true, will pull in market info (IOT to USD, IOT to BTC, etc)
     // *** Leave blank if you do not want market into pulled in ***
     market_info_flag: 'true'
+    
+    // window for bucketing confirmation times by transaction
+    // represents seconds and aggregates a histogram
+    confirm_time_buckets: [300, 600, 1200, 2400, 3600, 7200, 21600, 43200],
+
+    // how often the prune schedule will run (leave at default unless you have
+    // a good reason)
+    prune_interval_days: 1,
+
+    // number of days to keep confirmation stats
+    // db does not grow that quickly but checking the db/ folder
+    // size regularly will prevent any issues
+    // **Comment out to remove pruning**
+    retention_days: 14
 }
